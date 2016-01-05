3f972719793542237ac5f568dccee54777fce36c
Dimitar Valchanov <cerrutito@gmail.com>
2016-01-05 16:05:54 +0200

    Add comments for the used constants

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,14 +1,24 @@
+/**
+ * Directions.
+ */
 export const UP = 0;
 export const LEFT = 1;
 export const DOWN = 2;
 export const RIGHT = 3;
 
+/**
+ * Direction vectors in the form of `from -> to`.
+ * If the value is 0, there is no movement on that axis.
+ */
 export const VECTORS = [];
 VECTORS[UP] = {x: 1, y: 0};
 VECTORS[LEFT] = {x: 0, y: 1};
 VECTORS[DOWN] = {x: -1, y: 0};
 VECTORS[RIGHT] = {x: 0, y: -1};
 
+/**
+ * Key codes for the 4 arrows directions.
+ */
 export const DIRECTIONS = {
   38: UP,
   37: LEFT,
@@ -16,6 +26,9 @@ export const DIRECTIONS = {
   39: RIGHT
 };
 
+/**
+ * Game specific values.
+ */
 export const INITIAL = 2;
 export const SIZE = 4;
 export const STARTING_TILES = 2;
